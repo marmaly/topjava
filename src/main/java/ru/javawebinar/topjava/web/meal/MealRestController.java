649fd4179b4438db50b7df2ec6de428242c79071
marmaly <ej13666@mail.ru>
2017-11-28 13:39:40 +0500

    HW2_1

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.web.meal;
+
+import ru.javawebinar.topjava.service.MealService;
+
+/**
+ * Created by grh on 11/28/17.
+ */
+public class MealRestController {
+
+    private MealService service;
+}
--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++
@@ -1,9 +0,0 @@
-package ru.javawebinar.topjava.web.meal;
-
-/**
- * Created by grh on 11/28/17.
- */
-public class UserMealRestController {
-
-    U
-}
