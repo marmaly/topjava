54cbe1006e2839a35afae856e2ef36450a157706
marmaly <ej13666@mail.ru>
2017-12-05 16:53:20 +0500

    4 0 fix

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -49,6 +49,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void update(User user) {
-        repository.save(user);
+
+        checkNotFoundWithId(repository.save(user), user.getId());
     }
 }
